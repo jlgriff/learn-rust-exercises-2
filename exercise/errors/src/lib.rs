d0ddaa865d87c4cdc924453967ea09b7413319ff
John Griffith <john@John-MacBook-Air.local>
2022-07-04 12:17:29 -0400

    Completed errors exercise.

--- exercise/errors/src/lib.rs
+++ exercise/errors/src/lib.rs
@@ -14,7 +14,18 @@
 // Once you have completed defining the error type correctly, you should be able to run
 // `cargo build --lib` without any build errors or warnings. Then go to main.rs and continue with #2
 
-// pub enum DolphinError...
+use thiserror::Error;
+
+#[non_exhaustive]
+#[derive(Debug, Error)]
+pub enum DolphinError {
+    #[error("Dolphin is hungry")]
+    Hungry,
+    #[error("Dolphin is too young")]
+    TooYoung,
+    #[error("Dolphin's name is too long")]
+    LongName,
+}
 
 pub struct Dolphin {
     pub name: String,
